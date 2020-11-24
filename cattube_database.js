f62f00d1197b9595a2152e6d306bfada382dc733
CatWorldWeb <65866070+CatWorldWeb@users.noreply.github.com>
2020-11-24 20:03:34 +0300

    Add files via upload

---
+++ cattube_database.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAAre7sBkOs4BHwqngR3yb66mYtUIdw2Mg",
+    authDomain: "rpg1-ccb6a.firebaseapp.com",
+    databaseURL: "https://rpg1-ccb6a.firebaseio.com",
+    projectId: "rpg1-ccb6a",
+    storageBucket: "rpg1-ccb6a.appspot.com",
+    messagingSenderId: "209902203223",
+    appId: "1:209902203223:web:18c9860490a678b9ca0e12",
+    measurementId: "G-SSMJY6M541"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
